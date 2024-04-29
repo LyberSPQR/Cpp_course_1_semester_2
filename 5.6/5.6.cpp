ec192c64bebb2364e86faad210f846e28969d039
Lyber_SPQR <artuom0405@gmail.com>
2024-04-29 13:48:03 +0300

    add 5.7

--- 5.6/5.6.cpp
+++ 5.6/5.6.cpp
@@ -2,13 +2,32 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
-int main()
+int main() 
 {
-	vector<string> Text;
-	string data;
-}
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
 
+    if (!inputFile.is_open() || !outputFile.is_open()) {
+        cout << "Не удалось открыть файл." << endl;
+        return 0;
+    }
+    vector<string> words;
+    string word;
+
+    while (inputFile >> word)
+        words.push_back(word);
+
+    inputFile.close();
+
+    for (auto iterator = words.rbegin(); iterator != words.rend(); ++iterator) {
+        outputFile << *iterator << " ";
+    }
+    outputFile.close();
+    return 0;
+}
---
+++ 5.7/5.7.cpp
@@ -0,0 +1,46 @@
+﻿// 5.7.cpp
+//
+
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	list<string> Text;
+ 
+        ifstream inputFile("input.txt");
+        ofstream outputFile("output.txt");
+
+        if (!inputFile.is_open() || !outputFile.is_open()) {
+            cout << "Не удалось открыть файл." << endl;
+            return 0;
+        }
+        string word;
+        string temp;
+
+        while (inputFile >> word)
+            Text.push_back(word);
+
+        inputFile.close();
+
+        for (auto iterator = Text.begin(); iterator != Text.end(); iterator++)
+        {
+            temp = *iterator;
+            if (temp[0] == 'a')
+            {
+                for (int i = 0; i < temp.length(); i++)
+                {
+                    if ((97 <= temp[i] && temp[i] <= 122) || (65 <= temp[i] && temp[i] <= 90))
+                    {
+                        outputFile << temp[i];
+                    }
+                }
+                outputFile << " ";
+            } 
+        }
+        outputFile.close();
+        return 0;
+}
